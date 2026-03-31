f9b81efec5dc4138cadcd71e7bbb5ce7ef10b923
krishnasanjayj <krishnasanjayjeyakumar@gmail.com>
2026-03-31 12:32:31 +0530

    Remove sensitive API key from main.jsx
    Removed sensitive API key from main.jsx file.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-const API_KEY = "123456789SECRET";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
