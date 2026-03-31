478107c9417a674c1755cd790d86ed99721ed0a3
krishnasanjayj <krishnasanjayjeyakumar@gmail.com>
2026-03-31 12:19:35 +0530

    code smell

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+const API_KEY = "123456789SECRET";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
